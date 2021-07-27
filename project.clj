0e90819ddd80009f8bf8e20f589ee8760b234f8d
NPException <npexception42@gmail.com>
2021-07-27 19:40:01 +0200

    init basic project structure

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject spacetraders-clj-wrapper "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.10.2"]
+                 [org.clojure/data.json "2.4.0"]
+                 #_[org.clojure/core.async "1.2.603"]
+                 [com.github.ben-manes.caffeine/caffeine "3.0.3"]
+                 [http-kit "2.5.3"]]
+  :repl-options {:init-ns de.npcomplete.spacetraders-wrapper.core}
+  :source-paths ["src"]
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[criterium "0.4.5"]]}})
---
+++ src/de/npcomplete/spacetraders_wrapper/core.clj
@@ -0,0 +1,3 @@
+(ns de.npcomplete.spacetraders-wrapper.core
+  (:require [org.httpkit.client :as http]
+            [clojure.data.json :as json]))
